1e9b3a37069a62d1b8f2f18c453a5802f470ded7
unknown <souzarebeca997@gmail.com>
2021-06-01 17:42:07 -0300

    msg

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+serviceWorker.unregister();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +24,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
