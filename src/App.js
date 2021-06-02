7c1be9693b2697b0d1d6da03ab635065016bb68f
unknown <souzarebeca997@gmail.com>
2021-06-01 21:38:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import  "bootstrap/dist/css/bootstrap.min.css";
-
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AddTutorial from "./components/AddTutorial";
+import Tutorial from "./components/Tutorial";
+import TutorialsList from "./components/TutorialsList";
 
 function App() {
   return (
-    <div className="App">
+      <div className="App">
       <div className="container mt-3">
         <Switch>
           <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
